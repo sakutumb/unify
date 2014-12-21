cab34f22ad5fb2f0cfb466cd111b1c2b41350f80
Syam <syam.prasad@gmail.com>
2014-12-20 20:59:11 -0800

    Merge to stage branch

---
+++ app/assets/javascripts/controllers/HeroCtrl.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('UnifyApp').
+    controller('HeroCtrl', ['$scope', '$rootScope', '$location', '$anchorScroll', '$timeout', 'appConstants',
+        function ($scope, $rootScope, $location, $anchorScroll, $timeout, appConstants) {
+
+    }]);
+
