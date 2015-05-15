d8c94125120aa221b6e4118d06cc8cf3d4302cca
Andrei Hodecker <andreih@outlook.com>
2015-05-14 22:11:12 -0300

    creating list directive and validation

--- angular_app/assets/controllers/product/AccountCtrl.js
+++ angular_app/assets/controllers/product/AccountCtrl.js
@@ -4,9 +4,38 @@ angular.module('UnifyApp').
   controller('AccountCtrl', ['$scope',
     function ($scope) {
 
+      $scope.account = {}
+      $scope.account.languages = [];
+      $scope.account.community = [];
+
+      $scope.langOptions = [];
+      $scope.communityOptions = [];
+
       $scope.submit = function (account) {
-        
+        console.log(account);
+        // Code to submit form
       }
+
+      $scope.loadLanguages = function () { // Dummy function to load languages
+        $scope.langOptions = 
+          [
+            {"id": "en", "name": " English"},
+            {"id": "pt", "name": " Portugues"},
+            {"id": "sp", "name": " Spanish"}
+          ];
+      }
+
+      $scope.loadCommunities = function () { // Dummy function to load communities
+        $scope.communityOptions = 
+          [
+            {"id": "afrikanas", "name": " Afrikanas"},
+            {"id": "albanian", "name": " Albanian"},
+            {"id": "arabic", "name": " Arabic"}
+          ];
+      }
+
+      $scope.loadCommunities();
+      $scope.loadLanguages();
 
  		}
   ]);
--- angular_app/assets/directives/directives.js
+++ angular_app/assets/directives/directives.js
@@ -18,3 +18,52 @@ angular.module('UnifyApp').directive("compareTo", function() {
         }
     };
 });
+
+// Directive to create a list of checkbox with validation
+angular.module('UnifyApp').directive('checkboxList', function () {
+    return {
+        restrict: 'A',
+        replace: true,
+        require: 'ngModel',
+        scope: {
+            items: '=',
+            value: '@',
+            label: '@'
+        },
+        template: '<span><label ng-repeat="item in list">' +
+            '<input type="checkbox" value="{{item.value}}" ng-checked="item.checked" ng-click="toggle($index)"/>' +
+          '{{item.label}}' +
+          '</label></br></span>',
+        link: function(scope, elm, attrs, ctrl) {
+
+            ctrl.$validators.required =  function(modelValue) {
+                return modelValue.length > 0;
+            };
+
+            scope.toggle = function (index) {
+                ctrl.$setTouched();
+                var item = scope.list[index],
+                  i = ctrl.$viewValue.indexOf(item.value);
+                item.checked = !item.checked;
+                if (!item.checked && i > -1) {
+                  ctrl.$viewValue.splice(i, 1);
+                } else if (item.checked && i < 0) {
+                  ctrl.$viewValue.push(item.value);
+                }
+                ctrl.$validate();
+            };
+            scope.$watch('items', function (value) {
+                scope.list = [];
+                if (angular.isArray(value)) {
+                    angular.forEach(value, function (item) {
+                        scope.list.push({
+                            value: item[scope.value],
+                            label: item[scope.label],
+                            checked: ctrl.$modelValue.indexOf(item[scope.value]) > -1
+                        });
+                    });
+                }
+            }, true);
+        }
+    };
+});
