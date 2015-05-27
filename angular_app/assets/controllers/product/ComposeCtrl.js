539260536c5110e30b180c89a3a3d204a85654bc
Andrei Hodecker <andreih@outlook.com>
2015-05-27 20:40:12 -0300

    adding the validation to compose form

--- angular_app/assets/controllers/product/ComposeCtrl.js
+++ angular_app/assets/controllers/product/ComposeCtrl.js
@@ -3,11 +3,22 @@
 angular.module('UnifyApp').
   controller('ComposeCtrl', ['$scope', 'UnifyProductService',
     function($scope, UnifyProductService) {
+      
+      $scope.compose = {};
 
       $scope.contactOptions = UnifyProductService.getComposeContacts();
 
       $scope.send = function(){
-        console.log($scope.contacts);
+        $scope.$broadcast('show-errors-check-validity');
+        if ($scope.form.$valid) {
+          console.log($scope.compose);
+
+          // Code to submit form
+        }        
+      }
+
+      $scope.cancel = function(){
+    
       }
       
     }
--- angular_app/assets/directives/directives.js
+++ angular_app/assets/directives/directives.js
@@ -88,4 +88,25 @@ angular.module('UnifyApp')
             }
         );
     };
-});
+})
+
+// Directive to upload file
+// See more here https://stackoverflow.com/questions/17063000/ng-model-for-input-type-file
+.directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                var reader = new FileReader();
+                reader.onload = function (loadEvent) {
+                    scope.$apply(function () {
+                        scope.fileread = loadEvent.target.result;
+                    });
+                }
+                reader.readAsDataURL(changeEvent.target.files[0]);
+            });
+        }
+    }
+}]);
