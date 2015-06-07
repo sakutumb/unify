b7451bd78dada58fe11d283e0aa2ff313ca41a31
Andrei Hodecker <andreih@outlook.com>
2015-06-07 14:26:53 -0300

    search created

--- angular_app/assets/controllers/product/ProductSearchCtrl.js
+++ angular_app/assets/controllers/product/ProductSearchCtrl.js
@@ -4,10 +4,50 @@ angular.module('UnifyApp').
   controller('ProductSearchCtrl', ['$scope', '$state', 'UnifyProductService',
     function($scope, $state, UnifyProductService) {
 
+      $scope.search = {};
+
+      $scope.search.results = [];
+
+      $scope.activePage = 1;
+
+      $scope.clientList = UnifyProductService.getClientsList();
+
+      $scope.searchForById = function(clientId) {
+        $scope.search.searchingFor = UnifyProductService.searchForById(clientId);
+        $scope.activePage = 1;
+        $scope.queryResults($scope.activePage);
+      }
+
+      $scope.searchForByName = function(name) {
+        $scope.search.searchingFor = UnifyProductService.searchForByName(name);
+        $scope.activePage = 1;
+        $scope.queryResults($scope.activePage);
+      }
+
+      $scope.queryResults = function(page){
+        // Execute the search using the preferences
+        // of the client in $scope.search.searchingFor
+        var obj = UnifyProductService.querySearch(page, $scope.search.searchingFor);
+        $scope.search.pages = obj.pages;
+        $scope.search.results = obj.results;
+      }
+
+      $scope.viewProfile = function(client) {
+        // View client profile
+      }
+
+      $scope.shortlistResult = function(result) {
+        // Shortlist a result
+      }
+
       $scope.closeRefine = function () {
         //Reset the height of the sidebar
         $(".sidebar-search-list").css('height' , "");
         $state.transitionTo('product.search');
+      }
+
+      $scope.getPages = function(num) {
+        return new Array(num);   
       }
 
     }
--- angular_app/assets/services/unify_product_service.js
+++ angular_app/assets/services/unify_product_service.js
@@ -95,6 +95,56 @@ angular.module('UnifyApp')
         return array;
   		},
 
+      /** Search Tab **/
+      searchForById : function(id){
+        // Query the id here
+        return {
+          profile_picture: null,
+          name: "Michelle Wong",
+          place: "San Francisco, United States",
+          preferences: {
+            age: "30-40 Years Old",
+            ethnicity: "Hispanic, Asian, White",
+            location: "San Francisco, United States",
+            faith: "Christian (Catholic)",
+            build: "Slim, Curvy"
+          }
+        }
+      },
+      searchForByName : function(name){
+        // Query the name here
+        return {
+          profile_picture: null,
+          name: "Michelle Wong",
+          place: "San Francisco, United States",
+          preferences: {
+            age: "30-40 Years Old",
+            ethnicity: "Hispanic, Asian, White",
+            location: "San Francisco, United States",
+            faith: "Christian (Catholic)",
+            build: "Slim, Curvy"
+          }
+        }
+      },
+      querySearch : function(page, client){
+        // Query the client and the page here
+        var array = new Array();
+        array = 
+          [
+            {id: 1, profile_picture: null, name: "Michelle Wong", place: "San Francisco, United States"},
+            {id: 1, profile_picture: null, name: "Michelle Wong", place: "San Francisco, United States"},
+            {id: 1, profile_picture: null, name: "Michelle Wong", place: "San Francisco, United States"},
+            {id: 1, profile_picture: null, name: "Michelle Wong", place: "San Francisco, United States"},
+            {id: 1, profile_picture: null, name: "Michelle Wong", place: "San Francisco, United States"},
+            {id: 1, profile_picture: null, name: "Michelle Wong", place: "San Francisco, United States"},
+            {id: 1, profile_picture: null, name: "Michelle Wong", place: "San Francisco, United States"},
+            {id: 1, profile_picture: null, name: "Michelle Wong", place: "San Francisco, United States"},
+            {id: 1, profile_picture: null, name: "Michelle Wong", place: "San Francisco, United States"}
+          ];
+        return {pages: 5, results: array};
+      },
+
+
       /** Inbox Tab  **/
       getComposeContacts : function(){
         var array = new Array();
