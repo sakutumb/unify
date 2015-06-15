c08e01d3aae255dea45bec3e953072e61431bb9d
Andrei Hodecker <andreih@outlook.com>
2015-06-15 13:42:37 -0300

    fixng the problem with the height of the appearance

--- angular_app/assets/jquery/searchscript.js
+++ angular_app/assets/jquery/searchscript.js
@@ -1,6 +1,6 @@
 function calculateSearch() {
   $(".search-basic").css('height' , ($(".user-info.no-top-margin.standart-white-block").outerHeight()) * 3.15);
-  $(".search-appereance").css('height' , ($(".user-info.no-top-margin.standart-white-block").outerHeight()) * 2.06);
+  $(".search-appereance").css('height' , ($(".user-info.no-top-margin.standart-white-block").outerHeight()) * 2.25);
   $(".search-habits").css('height' , ($(".user-info.no-top-margin.standart-white-block").outerHeight()) * 2.06);
   $(".search-horoscope").css('height' , ($(".user-info.no-top-margin.standart-white-block").outerHeight()) * 2.06);
   $(".sidebar-search-list").css('height' , $(".user-info-select.no-top-margin.standart-white-block").height() - 33 );
